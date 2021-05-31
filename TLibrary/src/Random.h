4c457b08f54bbe5a5849722020541577bed7911d
Tiago Marino <tmsilva11@gmail.com>
2021-05-30 22:28:44 -0300

    Add InsideUnitCircle function

--- TLibrary/src/Random.h
+++ TLibrary/src/Random.h
@@ -1,10 +1,12 @@
 #pragma once
 #include <stdlib.h>
+#include "tvector.h"
 
 namespace Random 
 {
 	// Returns a random float within [0.0..1.0] (range is inclusive) (Read Only).
-	inline float Value() {
+	inline float Value() 
+	{
 		return (rand() / (float)RAND_MAX);
 	}
 
@@ -20,7 +22,12 @@ namespace Random
 		srand(seed);
 	}
 
-	//insideUnitCircle	Returns a random point inside or on a circle with radius 1.0 (Read Only).
+	//Returns a random point inside or on a circle with radius 1.0 (Read Only).
+	inline TLibrary::vec2 insideUnitCircle() 
+	{
+		float angle = Range(0.0f, 2 * T_PI);
+		return TLibrary::vec2(cosf(angle), sinf(angle));
+	}
 	//insideUnitSphere	Returns a random point inside or on a sphere with radius 1.0 (Read Only).
 	//onUnitSphere	Returns a random point on the surface of a sphere with radius 1.0 (Read Only).
 	//rotation	Returns a random rotation(Read Only).
